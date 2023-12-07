016f32b744563ef163d03d4bb11061e6673ab0db
bochrap <131978926+bochrap@users.noreply.github.com>
2023-12-07 15:10:59 +0000

    add clicke event listener and update cookie functions

---
+++ app.js
@@ -0,0 +1,20 @@
+const cookie = document.getElementById("theCookie");
+
+let cookieCounter = 0;
+let cps = 1;
+
+//Cookie clicked event listnener
+cookie.addEventListener("click", function () {
+  updateCookies(1);
+});
+
+//function updating the cookies count and cps by a given value
+function updateCookies(byHowMany) {
+  cookieCounter += byHowMany;
+  const howManyCookies = document.getElementById("numberOfCookies");
+  const howFastCookies = document.getElementById("cookiesPerSecond");
+  howManyCookies.textContent = cookieCounter;
+  howFastCookies.textContent = `${cps} cps`;
+}
+
+//Add cookies with time
